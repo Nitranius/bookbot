e11c48848a21cb4698d4d28c5f1f4b61b2e7af98
Nitranius <Matt_thejoker@hotmail.com>
2024-08-03 16:51:35 -0400

    added get words returns lower case text

--- main.py
+++ main.py
@@ -1,14 +1,26 @@
 def main():
-
-    with open("books/frankenstein.txt") as f:
-        file_contents = f.read()
-    count_words(file_contents)
-
-def count_words(file_contents):
-    words = len(file_contents.split())
+    book_path = "books/frankenstein.txt"
+    text = get_book_text(book_path)
+    num_words = get_num_words(text)
+    lowered_string = get_words(text)
+    print(f"{num_words} words found in the document")
     
-        
-    print (f"{words}")
 
+
+def get_num_words(text):
+    words = text.split()
+    return len(words)
+
+def get_words(words):
+    words_string = "".join(words)
+    lowered_string = words_string.lower()
+    return lowered_string
+
+def get_book_text(path):
+    with open(path) as f:
+        return f.read()
+
+
+    
 
 main()
