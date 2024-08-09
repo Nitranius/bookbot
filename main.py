1e27a947caf1f0856b8c81a61b7e12c88d2f30da
Nitranius <Matt_thejoker@hotmail.com>
2024-08-08 21:15:50 -0400

    list of dicts of all key/value pairs alphanumerical

--- main.py
+++ main.py
@@ -4,7 +4,9 @@ def main():
     num_words = get_num_words(text)
     lowered_string = get_words(text)
     dict_character = character_count(lowered_string)
-
+    key_value_alpha = print_character_number(dict_character)
+    print(key_value_alpha)
+    
 def get_num_words(text):
     words = text.split()
     return len(words)
@@ -25,9 +27,14 @@ def character_count(lowered_string):
             dict_character[character] = 1
         else: 
             dict_character[character] += 1
-    print (f"{dict_character}")
-        
-        
+    return dict_character
+
+def print_character_number(dict_character):
+    key_value_alpha = []
+    for key, value in dict_character.items():
+        if key.isalpha():
+            key_value_alpha.append({key: value})
+    return key_value_alpha
 
 
 
