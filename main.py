f4ae0cc10775e767de4560989488dc5134121575
Nitranius <Matt_thejoker@hotmail.com>
2024-08-26 21:10:26 -0400

    character count string returnedgit status

--- main.py
+++ main.py
@@ -5,7 +5,10 @@ def main():
     lowered_string = get_words(text)
     dict_character = character_count(lowered_string)
     key_value_alpha = print_character_number(dict_character)
-    print(key_value_alpha)
+    string_charcount_list = make_string_KV_pairs(key_value_alpha)
+    individual_char_string = print_list_individual(string_charcount_list)
+    print(individual_char_string)
+    
     
 def get_num_words(text):
     words = text.split()
@@ -36,6 +39,18 @@ def print_character_number(dict_character):
             key_value_alpha.append({key: value})
     return key_value_alpha
 
+def make_string_KV_pairs(key_value_alpha):
+    string_charcount_list = []
+    for dict in key_value_alpha:
+        key, value = list(dict.items())[0]
+        make_string_char_count = (f"the {key} character was found {value} times")
+        string_charcount_list.append(make_string_char_count)
+    return string_charcount_list
 
+def print_list_individual(string_charcount_list):
+    result = ""
+    for individual in string_charcount_list:
+        result += (f"{individual}\n")
+    return result
 
 main()
